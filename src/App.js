d838e842602b643e826b01fefe53955e380369fd
naresh-codee <githubnaresh@gmail.com>
2024-08-28 15:36:12 +0530

    1st commit

--- src/App.js
+++ src/App.js
@@ -3,12 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      {/* <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        
-      {/* </header>  */}
-      
+    <div className="App"> 
       <h1>Hello world</h1>
     </div>
   );
