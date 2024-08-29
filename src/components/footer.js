2f2718904f35004ac2fb85d08d807a7c19c7dd68
naresh-codee <githubnaresh@gmail.com>
2024-08-29 21:03:54 +0530

    footer basic completed

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,60 @@
 
 const Footer = () =>{
     return(
-        <div>Footer</div>
+        <div >
+            
+            <footer className="cursor-pointer text-white bg-black w-screen flex justify-evenly ">
+                <div>
+                        <p className="text-2xl font-bold font-sans">SWIGGY</p>
+                    </div>
+
+                    <div>
+                        <h2 className="font-bold">Company</h2>
+                        <ul className="text-slate-400">
+                            <li>About</li>
+                            <li>Careers</li>
+                            <li>Team</li>
+                            <li>Swiggy One</li>
+                            <li>Swiggy Instamart</li>
+                            <li>Swiggy Genie</li>
+                        </ul>
+                    </div>
+
+                    <div>
+                        <div>
+                            <h2 className="font-bold">Contact Us</h2>
+                            <ul className="text-slate-400">
+                                <li>Help & Support</li>
+                                <li>Partner with us</li>
+                                <li>Ride with us</li>
+                            </ul>
+                        </div>
+
+                        <div>
+                            <h2 className="font-bold pt-8">Legal</h2>
+                            <ul className="text-slate-400">
+                                <li>Terms & Conditions</li>
+                                <li>Cookie Policy</li>
+                                <li>Privacy policy</li>
+                                <li>Investor Relations</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div>
+                        <h2 className="font-bold">We deliver to :</h2>
+                        <ul className="text-slate-400">
+                            <li>Bengaluru</li>
+                            <li>Gurgaon</li>
+                            <li>Delhi</li>
+                            <li>Mumbai</li>
+                            <li>Pune</li>
+                        </ul>
+                    </div>
+            </footer>
+        </div>
+            
+            
     );
 };
 
