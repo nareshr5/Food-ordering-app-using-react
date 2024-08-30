fead413220eef689fad1a77e37e8214eb19ce66e
naresh-codee <githubnaresh@gmail.com>
2024-08-30 15:09:19 +0530

    what's on your mind component created

--- src/components/body.js
+++ src/components/body.js
@@ -1,9 +1,14 @@
-
+// import whats_on_your_mind from "../utils/constant";
+import WhatsOnMind from "./whatsonmind";
 const Body = () =>{
     return(
+        <div className="h-96 w-3/4 m-auto mt-4  bg-slate-400  flex flex-col">
+            
+          {/* <img className="h-[50%]" alt="image_logo" scr="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/2/6ef07bda-b707-48ea-9b14-2594071593d1_Pizzas.png" /> */}
+
         <div>
-            Body
-          
+            <WhatsOnMind/>
+        </div>          
         </div>
     );
 };
---
+++ src/components/whatsonmind.js
@@ -0,0 +1,18 @@
+
+const WhatsOnMind = () =>{
+    return(
+    <div className="p-4 border-b-2 ">
+        <h1 className="font-bold text-2xl mb-4">What's on your mind?</h1>
+        <div className="flex">
+            <p className="w-36 h-[180px] bg-gray-600 mx-5 ">Image here</p>
+            <p className="w-36 h-[180px] bg-gray-600 mx-5 ">Image here</p>
+            <p className="w-36 h-[180px] bg-gray-600 mx-5 ">Image here</p>
+            <p className="w-36 h-[180px] bg-gray-600 mx-5 ">Image here</p>
+            <p className="w-36 h-[180px] bg-gray-600 mx-5 ">Image here</p>
+        </div>
+        <div className="border-gray my-9"></div>
+    </div>
+    );
+};
+
+export default WhatsOnMind;
