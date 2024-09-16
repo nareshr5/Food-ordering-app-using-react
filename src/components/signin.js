c59f4e5f4d1e7a8f6e3d3f41328a7dc2fb5f7e6a
naresh-codee <githubnaresh@gmail.com>
2024-09-16 23:27:06 +0530

    added preventdefault form submission

--- src/components/signin.js
+++ src/components/signin.js
@@ -3,7 +3,7 @@ const SignIn = () =>{
 
             <div className="bg-orange-500 h-screen">
             
-              <form className="absolute my-[10%] w-3/12 mx-auto left-0 right-0 py-6 bg-black flex flex-col bg-opacity-75 text-white">
+              <form  onSubmit={(e) => e.preventDefault()}   className="absolute my-[10%] w-3/12 mx-auto left-0 right-0 py-6 bg-black flex flex-col bg-opacity-75 text-white">
                 <h1 className="py-1 px-2 mx-3 my-1 text-2xl">Sign In</h1>
                 <input className="py-1.5 px-2 mx-4 my-3" type="email" placeholder="Email"/>
                 <input className="py-1.5 px-2 mx-4 my-3" type="password" placeholder="Password"/>
