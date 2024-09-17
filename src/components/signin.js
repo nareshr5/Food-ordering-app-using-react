3152d429d5ac00e6cbe862379557476f5a0ea945
naresh-codee <githubnaresh@gmail.com>
2024-09-17 10:42:11 +0530

    sign up logic added

--- src/components/signin.js
+++ src/components/signin.js
@@ -1,10 +1,13 @@
-import { useRef } from "react";
+import { useRef , useState} from "react";
 import validateFormData from "./validateformdata";
 
 const SignIn = () =>{
 
     const email = useRef(null);
     const password = useRef(null);
+    const name=useRef(null);
+
+    const [isSignType,setIsSigninType] = useState(true);
 
     const formSubmit=() =>{
         // console.log(email.current.value);
@@ -18,16 +21,22 @@ const SignIn = () =>{
 
     }
 
+    const toggleUser=() =>{
+        setIsSigninType(!isSignType);
+    }
+
 
     return(
 
             <div className="bg-orange-500 h-screen">
             
               <form  onSubmit={(e) => e.preventDefault()}   className="absolute my-[10%] w-3/12 mx-auto left-0 right-0 py-6 bg-black flex flex-col bg-opacity-75 text-white">
-                <h1 className="py-1 px-2 mx-3 my-1 text-2xl">Sign In</h1>
+                <h1 className="py-1 px-2 mx-3 my-1 text-2xl">{isSignType?"Sign In" : "Sign Up"}</h1>
+                { !isSignType && (<input  ref={name} className="py-1.5 px-2 mx-4 my-3 text-black" type="text" placeholder="Name"/>) }
                 <input ref={email}  className="py-1.5 px-2 mx-4 my-3 text-black" type="email" placeholder="Email"/>
                 <input ref={password}  className="py-1.5 px-2 mx-4 my-3 text-black" type="password" placeholder="Password"/>
-                <button  onClick={() => formSubmit()} className="py-1.5 px-2 mx-4 my-3 bg-red-700 rounded-lg">Submit</button>
+                <button  onClick={() => formSubmit()} className="py-1.5 px-2 mx-4 my-3 bg-green-800 rounded-lg">Submit</button>
+                <p className="py-1.5 px-2 mx-4 my-3 text-white cursor-pointer" onClick={()=>toggleUser()}>{isSignType? "If you are a new user- Sign up !": "If already registered user ? - sign In"}</p>
               </form>
             </div>
        
