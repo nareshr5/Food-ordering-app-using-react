43e1c85080e1c2e33176783b6617e03f1ddfb6de
naresh-codee <githubnaresh@gmail.com>
2024-10-01 21:59:20 +0530

    cart page

--- src/components/cart.js
+++ src/components/cart.js
@@ -1,5 +1,8 @@
 const Cart = ()=>{
+
+    // if the user is not signed in redirect the user to the signin page and then direct him to the cart page
     return(
+
     <div className="w-7/12 h-screen mx-auto mt-3 bg-slate-200">
         
    </div>
