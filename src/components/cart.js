f2a0357a13d5e7ff1ae7b61bfadb66d323ce5468
naresh-codee <githubnaresh@gmail.com>
2024-10-02 21:23:19 +0530

    cart page changes

--- src/components/cart.js
+++ src/components/cart.js
@@ -4,7 +4,10 @@ const Cart = ()=>{
     return(
 
     <div className="w-7/12 h-screen mx-auto mt-3 bg-slate-200">
+        <h1 className="bg-orange-500 font-bold text-white py-2"> Cart</h1>
         
+        <div className="bg-slate-500 w-11/12 h-96 mx-auto my-2"></div>
+
    </div>
    );
 }
