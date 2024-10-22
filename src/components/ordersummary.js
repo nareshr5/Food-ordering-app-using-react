c99ac31b9c295e9867ffe02f45c05b701db2b887
naresh-codee <githubnaresh@gmail.com>
2024-10-22 18:18:51 +0530

    order summary page

--- src/components/ordersummary.js
+++ src/components/ordersummary.js
@@ -2,11 +2,14 @@ const OrderSummary = () =>{
     return(
         <div className="mt-2 mx-2">
             <h1 className="text-2xl font-bold bg-orange-600 text-white p-2 ">Order Summary</h1>
-            <h2>Order details</h2>
-            <ul className="flex">
-                <li>Items - </li>
-                <li>Price</li>
-            </ul>
+            <div className="px-2 bg-gray-300">
+                <h2>Order details</h2>
+                <ul className="flex">
+                    <li>Items - </li>
+                    <li>Price</li>
+                </ul>
+            </div>
+            
         </div>
     );
 }
