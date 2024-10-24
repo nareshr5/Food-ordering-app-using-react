00b93a1bef8d3ac87a7b207b1dd32e87cfbac41d
naresh-codee <githubnaresh@gmail.com>
2024-10-24 11:46:11 +0530

    styling the summary page

--- src/components/ordersummary.js
+++ src/components/ordersummary.js
@@ -4,10 +4,15 @@ const OrderSummary = () =>{
             <h1 className="text-2xl font-bold bg-orange-600 text-white p-2  ">Order Summary</h1>
             <div className="px-2 bg-gray-300 w-full h-96 mt-2 ">
                 <h2>Order details</h2>
-                <ul className="flex">
-                    <li>Items - </li>
-                    <li>Price</li>
-                </ul>
+
+                <div className="bg-gray-500 w-full h-70">
+                    <ul className="flex p-2 font-semibold">
+                        <li>Items - </li>
+                        <li>Price</li>
+                    </ul>
+                </div>
+
+                
             </div>
             
         </div>
