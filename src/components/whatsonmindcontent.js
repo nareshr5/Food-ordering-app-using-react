fd1c1c56d888d264466182f66b166d36f6c51dae
naresh-codee <githubnaresh@gmail.com>
2024-11-25 21:16:25 +0530

    working on rendering of the whatonmind list

--- src/components/whatsonmindcontent.js
+++ src/components/whatsonmindcontent.js
@@ -2,8 +2,7 @@ import { useEffect , useState} from "react";
 import {whatsonmindcontent_api} from "../utils/constant";
 
 import Whatsonmindcard from "./whatsonmindcard";
-import { CDN_URL } from "../utils/constant";
-import rating from "../images/rating.png"
+
 
 
 const Whatsonmindcontent = () => {
@@ -25,7 +24,7 @@ const Whatsonmindcontent = () => {
         
     }
 
-    console.log(list);
+    // console.log(list);
 
     if(list.length==0) return null;
    
@@ -36,26 +35,24 @@ const Whatsonmindcontent = () => {
             <div className="text-3xl font-semibold my-2">{list[0].card.card.title}</div>
             <div className=" my-4 text-slate-500">{list[0].card.card.description}</div>
 
-            <div className="">
-                    {/* {list.map((item,i) =>(
+            <div className="flex ">
 
-                           i>=3 ?  <Whatsonmindcard /> : ""
-                    ))} */}
+                    {list.map((item,i) =>(
 
-                    <div>
-                        <img  src={CDN_URL+list[3]?.card?.card?.info?.cloudinaryImageId} alt="restaurant_image" className="w-[273px] h-[182px] rounded-xl"/>
-                    </div>
+                        (i>=3){
 
-                    <div className="pl-2 pt-1">
-                        <div className="w-[261px] h-[24px] font-bold text-lg">{list[3]?.card?.card?.info?.name}</div>
-                            <div className="flex items-center">
-                                <img className="rounded-full w-[20px] h-[20px] mr-1" src={rating} alt="rating_star"/>
-                                <div className="font-bold text-sm">{list[3]?.card?.card?.info?.avgRating+" "+"."+" "+list[3]?.card?.card?.info?.sla?.slaString}</div>
-                            </div>
-                            
-                            <div className=" text-slate-500">{list[3]?.card?.card?.info?.cuisines}</div>
-                            <div className=" text-slate-500">{list[3]?.card?.card?.info?.areaName}</div>
-                    </div>
+                            { <div className="mx-5">  
+                                <Whatsonmindcard  data={item?.card?.card?.info}/> 
+                            </div> }
+
+                        }
+
+                         
+                           
+                    ))}
+
+                   
+
             </div>
         </div>
     )
