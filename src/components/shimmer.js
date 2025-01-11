6bd5e34504cf9fdb9c93b9b3d962a06d9d9d043d
naresh-codee <githubnaresh@gmail.com>
2025-01-11 22:43:11 +0530

    shimmer UI

--- src/components/shimmer.js
+++ src/components/shimmer.js
@@ -1,10 +1,65 @@
+const Shimmer =() =>{
+    return (
+        
+        <div className="shimmerContainer  mt-[65px] mx-auto w-[1010px]  ">
 
 
-const Shimmer = () =>{
+            <div className="flex justify-evenly  bg-gradient-to-b from-gray-400 mb-8">
 
-    return(
-        <div>Shimmer Ui</div>
-    );
-};
+                 <div className="  m-[5px] w-36 h-[180px] p-[7px] border-[1px] border-solid border-black bg-gradient-to-r from-gray-300"></div>
+                 <div className="  m-[5px] w-36 h-[180px] p-[7px] border-[1px] border-solid border-black bg-gradient-to-l from-slate-100"></div>
+                 <div className="  m-[5px] w-36 h-[180px] p-[7px] border-[1px] border-solid border-black bg-gradient-to-r from-slate-300"></div>
+                 <div className="  m-[5px] w-36 h-[180px] p-[7px] border-[1px] border-solid border-black bg-gradient-to-l from-slate-100"></div>
+                 <div className="  m-[5px] w-36 h-[180px] p-[7px] border-[1px] border-solid border-black bg-gradient-to-r from-slate-300"></div>
+                 <div className="  m-[5px] w-36 h-[180px] p-[7px] border-[1px] border-solid border-black bg-gradient-to-l from-slate-100"></div>
+                 <div className="  m-[5px] w-36 h-[180px] p-[7px] border-[1px] border-solid border-black bg-gradient-to-r from-slate-300"></div>
+                 
+                 
+            
 
-export default Shimmer;
+            </div>
+
+
+
+            <div>
+                <p className="border border-b-2 border-gray-500 mx-4 my-2"> </p>
+            </div>
+
+
+
+            <div className="flex justify-evenly my-14 bg-gradient-to-b from-gray-400">
+
+                <div className=" bg-gray-100 m-[5px] w-[273px] h-[284px] p-[7px] border-[1px] border-solid border-black bg-gradient-to-r from-gray-300"></div>
+                <div className=" bg-gray-100 m-[5px] w-[273px] h-[284px] p-[7px] border-[1px] border-solid border-black bg-gradient-to-l from-slate-200"></div>
+                <div className=" bg-gray-100 m-[5px] w-[273px] h-[284px] p-[7px] border-[1px] border-solid border-black bg-gradient-to-r from-gray-300"></div>
+                <div className=" bg-gray-100 m-[5px] w-[273px] h-[284px] p-[7px] border-[1px] border-solid border-black bg-gradient-to-l from-slate-200"></div>
+                
+                
+            </div>
+
+            <div className="flex justify-evenly bg-gradient-to-b from-gray-400">
+
+                <div className=" bg-gray-100 m-[5px] w-[273px] h-[284px] p-[7px] border-[1px] border-solid border-black bg-gradient-to-r from-gray-300"></div>
+                <div className=" bg-gray-100 m-[5px] w-[273px] h-[284px] p-[7px] border-[1px] border-solid border-black bg-gradient-to-l from-slate-200"></div>
+                <div className=" bg-gray-100 m-[5px] w-[273px] h-[284px] p-[7px] border-[1px] border-solid border-black bg-gradient-to-r from-gray-300"></div>
+                <div className=" bg-gray-100 m-[5px] w-[273px] h-[284px] p-[7px] border-[1px] border-solid border-black bg-gradient-to-l from-slate-200"></div>
+                
+                    
+                    
+                    
+
+            </div>
+
+            <div>
+                <p className="border border-b-2 border-gray-500 mx-4 my-2"> </p>
+            </div>
+    
+    
+    
+   
+    </div>);
+    
+    
+    }
+    
+    export default Shimmer;
