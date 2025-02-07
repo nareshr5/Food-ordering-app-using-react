3db29a53d20151ce81c5fe8689a9421a70aa3ce6
naresh-codee <githubnaresh@gmail.com>
2025-02-07 10:47:01 +0530

    created a basic popup functionality

--- src/components/search.js
+++ src/components/search.js
@@ -1,5 +1,36 @@
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+
+
 const Search = () =>{
-    return(<div>Search</div>);
+
+    const [openpopup, setopenpopup] = useState(false);
+    
+    return(
+    
+        <div>
+            <button onClick={()=> {
+                setopenpopup(true);
+                console.log("clicked")
+
+            }}>click to open popup</button>
+
+
+            {openpopup && <div className="bg-slate-400 p-2 m-3 rounded-lg popup">
+                            <div className="flex flex-col justify-between">
+                                <p>popup message</p>
+                                <button onClick={()=>setopenpopup(false)}>X</button>
+                            </div>
+                            <div>
+                                <p>this is the message:</p>
+                                <NavLink to={"/cart"} >got to cart page</NavLink>
+                            </div>
+                          </div>
+
+            }
+           
+
+        </div>);
 }
 
 export default Search;
