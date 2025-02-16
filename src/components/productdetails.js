a2c9c1048b4be2335f4cd6be591082d1444e0c9a
naresh-codee <githubnaresh@gmail.com>
2025-02-16 11:40:14 +0530

    working on the product details on cart page

---
+++ src/components/productdetails.js
@@ -0,0 +1,67 @@
+import { useState ,useEffect} from "react";
+import { veg_logo} from "../utils/constant";
+
+
+const Productdetails = (props) =>{
+
+    
+    const {data} = props;
+    const {quantity}=data;
+    const {price}=data?.details?.card?.info;
+    const [count,setcount]=useState(data?.quantity)
+
+    console.log(data);
+
+    // useEffect(()=>{
+    //     fun((data?.details?.card?.info?.defaultPrice)/100);
+    // },[])
+
+  
+    
+    return(
+        <div>
+            <div className="px-[30px]">
+                <div className="py-[10px] flex justify-between">
+
+                    <div className="flex">
+                        <img className="w-7 h-7" src={veg_logo} alt="veg_icon" />
+                        <div className="ml-[5px] mr-[14px] ">{data?.details?.card?.info?.name}</div>
+                        
+                    </div>
+
+
+                    <div className="flex">
+                        <div className="border-2 border-slate-300 h-[32px] w-[70px] flex justify-around">
+                            <div><button onClick={() => {
+                           
+                            count!=1 && setcount(count-1)
+                            }} >-</button></div>
+
+                            
+                            <div>{count}</div>
+
+                            
+
+                            <div><button onClick={() =>{
+                                
+                                count>0 && setcount(count+1)
+
+                                
+                                
+                                
+                            }} >+</button></div>
+                        </div>
+                        <div className="ml-5"><span>₹{(price)/100*count}</span></div> 
+
+                        {/* <div className="ml-5"><span>₹{(item?.details?.card?.info?.defaultPrice)*(item?.quantity)}</span></div> */}
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+        
+    );
+};
+
+export default Productdetails;
