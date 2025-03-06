6558494a1f665b8a4724bb05a50968d6821f4371
naresh-codee <githubnaresh@gmail.com>
2025-03-06 17:33:11 +0530

    making the payment page dynamic and added some stylings

--- src/components/payment.js
+++ src/components/payment.js
@@ -7,45 +7,69 @@ import { useNavigate } from "react-router-dom";
 
 
 
+
 const Payment = () =>{
 
      const {paymentValue} = useSelector((store)=> store.payment);
     // console.log(toPay);
 
+    const {resList} = useSelector((store) => store.cart);
+
+    console.log(resList);
+
     const navigate=useNavigate();
 
-    return(
-        <div className="w-8/12 h-auto  mx-auto my-5  bg-slate-100">
-            <div className="mx-5 pb-[40px]">
-                <div className="font-bold my-5 text-2xl pt-5">Payment options </div>
+    const {deliverytime} = useSelector((store)=> store.cart)
 
-                 <div className="border-2 border-gray-600 p-4 rounded-3xl">
+    return(
+        <div className="w-8/12 h-auto  mx-auto my-5  bg-slate-100 ">
+            
+            <div className="pb-[40px]">
+                <div className="font-bold  text-2xl  my-5 bg-orange-600 text-white mt-5 flex items-center"><span className="px-6 py-4 ">Payment options</span> </div>
+
+                 <div className="border-2 border-gray-600 p-4 rounded-3xl mx-5">
                             <div className="flex">
-                                <div className="pr-2 text-gray-800 font-bold">Starbucks</div>
-                                <p className="mr-2 text-gray-500">| Delivery in: 41 mins</p>
+                                <div className="pr-2 text-gray-800 font-bold">{resList}</div>
+                                <p className="mr-2 text-gray-500">| Delivery in: {deliverytime}  mins</p>
                             </div>
 
 
-                            <div className="mt-2 flex">
+                            {/* <div className="mt-2 flex">
                                 <div className="pr-2 pb-1 text-gray-800 font-bold">Work</div>
                                 <div className="text-gray-500">| 11, House, 1st Main Rd, Peenya Iii Phase, Peenya, Bengaluru, Karnataka 560058, India</div>
-                            </div>
+                            </div> */}
                  </div>
 
 
+                 <div className="my-2 mx-auto w-3/4 mt-7 font-semibold">Review your order to avoid cancellations</div>
+                 <div className="w-3/4 h-20 bg-white mx-auto  py-4 px-5 rounded-2xl shadow-lg">
+                                
+                                    <div className="flex mx-3">
+                                    <span className="text-red-600 mr-2 font-semibold">NOTE: </span>
+                                    <span> Orders cannot be cancelled and are non-refundable once packed for delivery</span>
+                                    </div>
+
+                                    <span className="text-orange-600 font-semibold text-sm mt-3 mx-4">Read Cancellation Policy</span>
+                </div>
 
 
-                 <div className="border-2 border-orange-600 p-4 rounded-3xl mt-4 w-3/4 mx-auto">
+                 <div className=" p-4 rounded-3xl mt-4 w-3/4 mx-auto">
+
+                                
                             
-                            <div className="bg-green-500 text-white flex mx-auto w-[80%] p-2 rounded-2xl cursor-pointer ">
+                            <button onClick={()=> navigate("/summary") } className=" delay-100 duration-150 hover:scale-105 bg-green-500 text-white flex mx-auto w-[90%] p-2 rounded-2xl cursor-pointer ">
 
-                                <p className="font-bold text-2xl ml-28 " onClick={()=> navigate("/summary") }>Amount to be paid: </p>
+                                <p className="font-bold text-2xl ml-28 " >Amount to be paid: </p>
 
-                                <p className=" text-2xl font-semibold mx-4 text-black font-serif">{paymentValue}</p>
-                            </div>
+                                <p className=" text-2xl font-semibold mx-4 text-black font-serif">{paymentValue.toPrecision(3)}</p>
+                            </button>
                             <p className="mt-2 font-semibold w-[30%] mx-auto rounded-lg border-2 border-black">Select a payment option</p>
                  </div>
 
+
+                <div className="mt-16 h-[19px] mb-5 mx-7 text-slate-500">
+                    ---- *** the section below is just for representative purpose and doesnot hyperlink to actual working pages *** ---
+                </div>
 
                 <div className="mt-6 h-[19px] mb-5">
                             <h2 className="text-gray-600 px-5 mb-4 font-bold">Pay By an UPI App</h2>
@@ -62,7 +86,8 @@ const Payment = () =>{
 
 
                                 <div className=" h-[24px] w-[32px] border-[1px] border-slate-400 rounded-lg ">
-                                    <button className="text-orange-600 font-bold text-xl  w-[10px] h-[22px] px-[6px] -py-[4px] ">+</button>
+                                    {/* <button className="text-orange-600 font-bold text-xl  w-[10px] h-[22px] px-[6px]  -py-[6px] ">+</button> */}
+                                    <button className="text-orange-600 font-bold text-xl pl-1.5  h-[12px] -mt-1">+</button>
                                 </div>
 
                                 <div className="px-4">
@@ -101,7 +126,8 @@ const Payment = () =>{
 
 
                                 <div className=" h-[24px] w-[32px] border-[1px] border-slate-400 rounded-lg ">
-                                    <button className="text-orange-600 font-bold text-xl  w-[10px] h-[22px] px-[6px] ">+</button>
+                                    {/* <button className="text-orange-600 font-bold text-xl  w-[10px] h-[22px] px-[6px]  -py-[6px] ">+</button> */}
+                                    <button className="text-orange-600 font-bold text-xl pl-1.5  h-[12px] -mt-1">+</button>
                                 </div>
 
                                 <div className="px-4">
